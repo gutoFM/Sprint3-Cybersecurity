ad73c595e2359124290ccdd999efa15fb4003897
gutofm <gutofisco@gmail.com>
2025-09-21 03:05:18 -0300

    Dast changes

--- ci/run-dast.sh
+++ ci/run-dast.sh
@@ -8,7 +8,7 @@ PROJECT_PATH="Sprint3-CSHARP/CandidatosApi/SprintApi.csproj"
 OUTPUT_PATH="./docs/zap-report.html"
 API_URL="http://localhost:5000"
 
-echo "Subindo API em background"
+echo "Subindo API em background..."
 dotnet run --project $PROJECT_PATH --urls $API_URL &
 API_PID=$!
 
@@ -16,19 +16,19 @@ API_PID=$!
 echo "Aguardando API iniciar..."
 for i in {1..20}; do
   if curl -s $API_URL >/dev/null; then
-    echo "API está no ar em $API_URL"
+    echo "API está rodando em $API_URL"
     break
   fi
   sleep 2
 done
 
-# Rodar o OWASP ZAP baseline scan
-echo "Executando OWASP ZAP contra $API_URL..."
-docker run --rm -v $(pwd)/docs:/zap/wrk/:rw owasp/zap2docker-stable zap-baseline.py \
+echo "🔍 Executando OWASP ZAP contra $API_URL..."
+docker run --rm -v $(pwd)/docs:/zap/wrk/:rw zaproxy/zap-stable zap-baseline.py \
   -t $API_URL \
   -r zap-report.html
 
-# Finaliza a API
+
+# FInaliza a API
 kill $API_PID
 
 echo "Relatório gerado em $OUTPUT_PATH"

b755230f14f891d675e9c8676eef63f39a9baab3
gutofm <gutofisco@gmail.com>
2025-09-20 21:30:06 -0300

    Depcheck changes & SCA sh

--- ci/run-sca.sh
+++ ci/run-sca.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+# Detecta caminho do Java automaticamente
+if [ -z "$JAVA_HOME" ]; then
+  JAVA_PATH=$(readlink -f $(which java) | sed "s:/bin/java::")
+  export JAVA_HOME=$JAVA_PATH
+  export PATH=$JAVA_HOME/bin:$PATH
+fi
+
+echo "Usando JAVA_HOME=$JAVA_HOME"
+
 ./depcheck/dependency-check/bin/dependency-check.sh \
   --project "Sprint3-CSHARP" \
   --scan Sprint3-CSHARP/ \
